9646ab6d987789ad070c46e1a20dc04ca9a4a25d
blackshirt <blackshirt@fastmail.fm>
2013-04-05 10:51:51 +0700

    modify commit

--- webapt/commit.py
+++ webapt/commit.py
@@ -3,8 +3,10 @@
 import apt
 
 def commit():
-    return apt.Cache().commit(apt.progress.text.AcquireProgress(), \
-                              apt.progress.text.OpProgress())
+    cache = apt.Cache()
+    with cache.actiongroup():
+        for pakage in cache.get_changes():
+            return cache.commit(apt.progress.text.AcquireProgress(), apt.progress.text.OpProgress())
 
 if __name__ == '__main__':
     commit()
